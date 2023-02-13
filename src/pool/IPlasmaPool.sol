644c24e3aea5e91b644b19d366623ee39491c900
Cody Nutt <23013728+codenutt@users.noreply.github.com>
2023-02-13 11:02:27 -0600

    fix: add oz ref

--- src/pool/IPlasmaPool.sol
+++ src/pool/IPlasmaPool.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "../../node_modules/@openzeppelin/contracts/interfaces/IERC4626.sol";
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
+import "openzeppelin-contracts/interfaces/IERC4626.sol";
+import "openzeppelin-contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 
 // solhint-disable-next-line no-empty-blocks
 interface IPlasmaPool is IERC4626, IERC20Permit { }
--- src/pricing/EthValueOracle.sol
+++ src/pricing/EthValueOracle.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 import "./value-providers/IBaseValueProvider.sol";
 import "./IEthValueOracle.sol";
 
-import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
 contract EthValueOracle is IEthValueOracle, Ownable {
     function addProvider(address provider) external onlyOwner { }
