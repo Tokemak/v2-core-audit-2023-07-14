030e7363ea5f7745a72fc3745b54ebe4eb0afbcb
Jacob San Juan <contactjsj42@gmail.com>
2023-02-13 15:04:42 -0700

    chore: added chainlink registry interface

--- src/pricing/value-providers/ChainlinkValueProvider.sol
+++ src/pricing/value-providers/ChainlinkValueProvider.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "./IBaseValueProvider.sol";
+import "../../interfaces/pricing/IBaseValueProvider.sol";
+
+import "../../interfaces/external/IChainlinkFeedRegistry.sol";
 
 contract ChainlinkValueProvider is IBaseValueProvider {
+    IChainlinkFeedRegistry public registry;
+
+    error RegsitryAddressZero();
+
+    constructor(address _registry) public {
+        if (_registry == address(0)) {
+            revert RegsitryAddressZero();
+        }
+        registry = IChainlinkFeedRegistry(_registry);
+    }
+
     function getPrice(address token) external { }
 }
