599d62635474b5d69fffe72544ced562d0c2ffc5
Parker Burchett <57877709+parkerburchett@users.noreply.github.com>
2023-04-25 12:44:44 -0700

    feat: add more fields to  pool stats

--- src/poolstats/FakePool.sol
+++ src/poolstats/FakePool.sol
@@ -5,6 +5,15 @@ import { Stats } from "./PoolStats.sol";
 
 library FakePool {
     function getStats() public view returns (Stats.PoolStats memory) {
-        return Stats.PoolStats({baseApr: 1});
+        return Stats.PoolStats(
+            {
+                baseApr: 100,
+                feeApr: 1,
+                incentiveApr:1000,
+                marketPriceTvl: 1000,
+                underlyingPriceTvl: 1010,
+                lpTokenTotalSupply: 990,
+                timestamp: 1682451824,           
+            });
     }
 }
--- src/poolstats/PoolStats.sol
+++ src/poolstats/PoolStats.sol
@@ -4,5 +4,11 @@ pragma solidity 0.8.17;
 library Stats {
     struct PoolStats {
         uint32 baseApr;
+        uint32 feeApr;
+        uint32 incentiveApr;
+        uint32 marketPriceTvl;
+        uint32 underlyingPriceTvl;
+        uint32 lpTokenTotalSupply;
+        uint32 timestamp;
     }
 }
