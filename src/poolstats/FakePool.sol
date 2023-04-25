8cb5d5112956a62381cc9af743eac64a429842a9
Parker Burchett <57877709+parkerburchett@users.noreply.github.com>
2023-04-25 14:55:47 -0700

    feat: removed trailing comma

--- src/poolstats/FakePool.sol
+++ src/poolstats/FakePool.sol
@@ -5,15 +5,14 @@ import { Stats } from "./PoolStats.sol";
 
 library FakePool {
     function getStats() public view returns (Stats.PoolStats memory) {
-        return Stats.PoolStats(
-            {
-                baseApr: 100,
-                feeApr: 1,
-                incentiveApr:1000,
-                marketPriceTvl: 1000,
-                underlyingPriceTvl: 1010,
-                lpTokenTotalSupply: 990,
-                timestamp: 1682451824,           
-            });
+        return Stats.PoolStats({
+            baseApr: 100,
+            feeApr: 1,
+            incentiveApr: 1000,
+            marketPriceTvl: 1000,
+            underlyingPriceTvl: 1010,
+            lpTokenTotalSupply: 990,
+            timestamp: 1_682_451_824
+        });
     }
 }
