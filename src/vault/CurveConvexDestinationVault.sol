6d4e7123465776d1455a41be339f77efaed6d848
Hennadii Ivtushok <hivtushok@gmail.com>
2023-06-02 13:06:06 +0200

    fix(destination-vaults): Update licensing

--- src/vault/CurveConvexDestinationVault.sol
+++ src/vault/CurveConvexDestinationVault.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (c) 2023 Tokemak Foundation. All rights reserved.
 pragma solidity 0.8.17;
 
 import { Math } from "openzeppelin-contracts/utils/math/Math.sol";
