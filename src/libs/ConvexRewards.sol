c8d10ab1ca42ae1bc3a05dc61f453bbdedbc7e32
sam <samuel.hagan@gmail.com>
2023-06-28 16:06:32 -0700

    feat(stats): add convex reward mint library

---
+++ src/libs/ConvexRewards.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+// reference: https://docs.convexfinance.com/convexfinanceintegration/cvx-minting
+library ConvexRewards {
+    uint256 internal constant CVX_MAX_SUPPLY = 100_000_000 * 1e18; // 100 mil max supply
+    uint256 internal constant CLIFF_COUNT = 1000;
+    uint256 internal constant CLIFF_SIZE = CVX_MAX_SUPPLY / CLIFF_COUNT; // 100_000 per clif
+
+    /**
+     * @notice Calculates the amount of CVX that is minted given the amount of CRV earned
+     * @param cvxToken address for CVX token
+     * @param crvEarned the amount of CRV reward that was earned
+     */
+    function getCVXMintAmount(address cvxToken, uint256 crvEarned) internal view returns (uint256) {
+        uint256 cvxSupply = IERC20(cvxToken).totalSupply();
+
+        // if no cvx has been minted, pre-mine the same amount as the provided crv
+        if (cvxSupply == 0) {
+            return crvEarned;
+        }
+
+        // determine the current cliff
+        uint256 currentCliff = cvxSupply / CLIFF_SIZE;
+
+        // if the current cliff is below the max, then CVX will be minted
+        if (currentCliff < CLIFF_COUNT) {
+            uint256 remainingCliffs = CLIFF_COUNT - currentCliff;
+            uint256 cvxEarned = crvEarned * remainingCliffs / CLIFF_COUNT;
+
+            // ensure that the max supply has not been exceeded
+            uint256 amountUntilMax = CVX_MAX_SUPPLY - cvxSupply;
+            if (cvxEarned > amountUntilMax) {
+                // if maxSupply would be exceeded then return the remaining supply
+                return amountUntilMax;
+            }
+
+            return cvxEarned;
+        }
+
+        return 0;
+    }
+}
---
+++ test/libs/ConvexRewards.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (c) 2023 Tokemak Foundation. All rights reserved.
+pragma solidity 0.8.17;
+
+import { Test } from "forge-std/Test.sol";
+import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import { ConvexRewards } from "src/libs/ConvexRewards.sol";
+import { CVX_MAINNET } from "test/utils/Addresses.sol";
+
+contract ConvexRewardTest is Test {
+    function testCVXRewardHistoric() public {
+        // targeting this transaction:
+        // https://etherscan.io/tx/0x46314e9730ef79c43135de478daf04b08acec36095045376f46ec7920daef0bb
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 17_580_732);
+        uint256 crvEarned = 43_632_695_514_897_054_325;
+        uint256 expectedCVX = 698_123_128_238_352_869;
+
+        uint256 cvxAmount = ConvexRewards.getCVXMintAmount(CVX_MAINNET, crvEarned);
+
+        assertEq(cvxAmount, expectedCVX);
+    }
+
+    function testCVXRewardIfTotalSupplyIsZero() public {
+        checkCVXMintAmount(0, 987, 987);
+    }
+
+    function testCVXRewardIfInCliffs() public {
+        uint256 totalSupply = 101_000 * 1e18; // each cliff is 100_000 tokens, so we're in the 1st cliff (zero indexed)
+        uint256 crvEarned = 898_000;
+        uint256 expectedCVX = crvEarned * 999 / 1000;
+        checkCVXMintAmount(totalSupply, crvEarned, expectedCVX);
+    }
+
+    function testCVXRewardIfInLastCliff() public {
+        uint256 totalSupply = 99_999_000 * 1e18; // leaves 1000 tokens
+        uint256 crvEarned = 1001 * 1000 * 1e18; // try to exceed the max supply
+        uint256 expectedCVX = 1000 * 1e18; // expect to only get the remaining 1000 tokens, not 1001
+        checkCVXMintAmount(totalSupply, crvEarned, expectedCVX);
+    }
+
+    function checkCVXMintAmount(uint256 totalSupply, uint256 crvEarned, uint256 expectedCVXAmount) internal {
+        address cvx = vm.addr(totalSupply + crvEarned);
+        vm.mockCall(cvx, abi.encodeWithSelector(IERC20.totalSupply.selector), abi.encode(totalSupply));
+
+        uint256 cvxAmount = ConvexRewards.getCVXMintAmount(cvx, crvEarned);
+
+        assertEq(cvxAmount, expectedCVXAmount);
+    }
+}
